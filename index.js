65c61d1615d27f274729ffe5eec5a08743801009
Arghya Banerjee <arghyabanerjee@gmail.com>
2023-01-11 23:44:35 +0530

    Update the issues

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 //For Graviti Internship Assignment by Arghya Banerjee
 
+//Add waypoint function...
+var count = 1
+function addWaypoint() {
+    let WaypointArea = document.getElementById('Waypoints-field')
+    WaypointArea.insertAdjacentHTML("beforeend", `<input type="text" class="user_Input validator" name="" placeholder="Waypoint ${count + 1}" required>`)
+    count++
 
+}
 
 // Inin map from google maps
 function initMap() {
@@ -16,6 +23,36 @@ function initMap() {
         calculateAndDisplayRoute(directionsService, directionsDisplay);
     };
     document.getElementById('btn').addEventListener('click', onChangeHandler);
+
+
+    //Auto complete function for Origin....
+    var originInput = document.getElementById("origin")
+    var waypointInput = document.getElementById('waypoint')
+    var destInput = document.getElementById('dest')
+    const autocomplete = new google.maps.places.Autocomplete(originInput, {
+        types: ['geocode']
+    })
+
+    autocomplete.addListener('place_changed', function () {
+        var nearPlace = autocomplete.getPlace()
+    })
+
+    //Auto complete function for Destination....
+    const autocompleteDest = new google.maps.places.Autocomplete(destInput, {
+        types: ['geocode']
+    })
+
+    autocompleteDest.addListener('place_changed', function () {
+        var nearPlace = autocomplete.getPlace()
+    })
+    //Auto complete function for Waypoint....
+    const autocompleteWP = new google.maps.places.Autocomplete(waypointInput, {
+        types: ['geocode']
+    })
+
+    autocompleteWP.addListener('place_changed', function () {
+        var nearPlace = autocomplete.getPlace()
+    })
 }
 
 //Call the functions
@@ -29,18 +66,28 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
     //console.log(modeOfTravel)
     originName.innerHTML = document.getElementById('origin').value
     destName.innerHTML = document.getElementById('dest').value
+
+    //way points
     var waypoints1 = [];
+    var waypointArray = []
+    var WaypointsField = document.getElementById('Waypoints-field')
+    var lengthOfWayPoints = WaypointsField.children.length
+    // console.log(lengthOfWayPoints)
     var checkwaypoint = document.getElementById('waypoint').value
     if (checkwaypoint == "") {
         window.alert("No Stops found! Do you want to proceed?")
 
     } else {
-        waypoints1.push({
-            location: checkwaypoint,
-            stopover: true
-        })
-    }
 
+        for (let i = 0; i < lengthOfWayPoints; i++) {
+            waypoints1.push({
+                location: WaypointsField.children[i].value,
+                stopover: true
+            })
+
+            waypointArray.push(WaypointsField.children[i].value)
+        }
+    }
 
     directionsService.route({
         origin: document.getElementById('origin').value,
@@ -51,8 +98,8 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
     }, function (response, status) {
         if (status === 'OK') {
             directionsDisplay.setDirections(response);
-            console.log(response.routes[0].legs.length)
-            var distanceRawData
+            //console.log(response.routes[0].legs.length)
+            var distanceRawData = 0
             var durationRawData
 
             if (checkwaypoint == "") {
@@ -63,39 +110,66 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
                 calDistance.innerHTML = `${distance} <span id="kms">kms</span>`
                 calDuration.innerHTML = `${durationRawData}`
                 distValue2.innerHTML = `${distance} kms.`
-
             }
             else {
-                distanceRawData = response.routes[0].legs[0].distance.value + response.routes[0].legs[1].distance.value
-                // Extracting the ETA
-                durationLeg0 = response.routes[0].legs[0].duration.text
-                durationLeg1 = response.routes[0].legs[1].duration.text
+                var i
+                // Extracting the total Distance
+                for (i = 0; i <= waypoints1.length; i++) {
+                    distanceRawData += response.routes[0].legs[i].distance.value
+                }
 
-                calDuration.innerHTML = `${document.getElementById('origin').value} To ${checkwaypoint}: ${durationLeg0} ${checkwaypoint} To ${document.getElementById('dest').value} : ${durationLeg1}`
-                document.getElementById('result-parameter2').className = "specialCase"
-                //document.getElementById('result-parameter2').style.fontSize = "5px"
+                // Extracting the ETA
+                var duration = []
+                for (i = 0; i <= waypoints1.length; i++) {
+                    duration.push(response.routes[0].legs[i].duration.text)
+                }
+                //Print duration
+                for (i = 0; i <= duration.length; i++) {
+                    console.log(duration[i])
+                }
+
+                if (waypoints1.length > 1) {
+                    calDuration.innerHTML = `${document.getElementById('origin').value.toUpperCase()} To ${waypoints1[0].location.toUpperCase()} : ${duration[0]} <br>`
+
+                    for (i = 1; i < waypointArray.length; i++) {
+                        //console.log(duration[i]);
+                        calDuration.insertAdjacentHTML("beforeend", `${waypointArray[i - 1].toUpperCase()} To ${waypointArray[i].toUpperCase()} : ${duration[i]} <br>`)
+                    }
+                    calDuration.insertAdjacentHTML("beforeend", `${waypointArray[waypointArray.length - 1].toUpperCase()} To ${document.getElementById('dest').value.toUpperCase()} : ${duration[duration.length - 1]}`)
+                    document.getElementById('result-parameter2').className = "specialCase"
+                } else {
+                    calDuration.innerHTML = `${document.getElementById('origin').value.toUpperCase()} To ${checkwaypoint.toUpperCase()} : ${duration[0]}  <br> ${checkwaypoint.toUpperCase()} To ${document.getElementById('dest').value.toUpperCase()} : ${duration[1]}`
+                }
 
                 let distance = Math.floor(distanceRawData / 1000)
                 calDistance.innerHTML = `${distance} <span id="kms">kms</span>`
                 distValue2.innerHTML = `${distance} kms.`
             }
-
-            console.log(durationRawData)
-
-        } 
-        //If errors
+            // console.log(durationRawData)
+        }
+        //Errors handling
         else {
-            if (status == "ZERO_RESULTS") {
-                window.alert(`Travelling can not be covered by ${modeOfTravel} mode. Please change the mode of travelling.`)
+            if (status == "ZERO_RESULTS" && modeOfTravel != "BICYCLING") {
+                window.alert(`No route could be found between the ${document.getElementById('origin').value} and ${document.getElementById('dest').value} by ${modeOfTravel} mode.`)
             }
+
+            else if (status == "ZERO_RESULTS" && modeOfTravel == "BICYCLING") {
+                window.alert(`${modeOfTravel} mode currently available only in the US and some Canadian cities`)
+            }
+
             else if (status == "INVALID_REQUEST") {
                 window.alert(`Distance can not covered by ${modeOfTravel} mode. You need to take Flight to reach that place.`)
             }
-            else {
 
+            else if (status == "NOT_FOUND") {
+                window.alert(`Please enter a valid Place!!`)
+            }
+
+            else {
                 window.alert('Directions request failed due to ' + status);
             }
         }
     });
 }
+
 
